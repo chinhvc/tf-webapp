c4390537425d134414e43ac61045aaa2725feafb
moabukar <mo-abukar10@hotmail.com>
2023-04-08 06:41:40 +0100

    add tests for VPC

--- test/web_test.go
+++ test/web_test.go
@@ -7,6 +7,30 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
+func TestVPCModule(t *testing.T) {
+	t.Parallel()
+
+	awsRegion := "us-west-2"
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../vpc",
+
+		Vars: map[string]interface{}{
+			// Add any required variables here
+		},
+
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Add your assertions here
+}
+
 func TestWebAppModule(t *testing.T) {
 	// Set up Terraform options.
 	terraformOptions := &terraform.Options{
