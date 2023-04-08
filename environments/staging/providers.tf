f73bc36000a4814491c72d2b04177f2da947dc6f
moabukar <mo-abukar10@hotmail.com>
2023-04-08 07:13:24 +0100

    add tf block provider

--- environments/staging/providers.tf
+++ environments/staging/providers.tf
@@ -1,10 +1,21 @@
+terraform {
+  required_version = "~> 1.2.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.13"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
 
 terraform {
   backend "s3" {
-    bucket = "my-unique-bucket-name"
+    bucket = "staging-bucket"
     key    = "dev/terraform.tfstate"
     region = "us-west-2"
   }
